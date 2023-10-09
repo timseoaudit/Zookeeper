7d6d0c75fcd9451a3c45e89818f92f796e60d9b7
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-10-09 02:31:27 +0100

    Initial Commit

---
+++ Zookeeper/ZookeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZookeeperApp.swift
+//  Zookeeper
+//
+//  Created by user on 09/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ZookeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
